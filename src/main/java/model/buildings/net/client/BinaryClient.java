e9ec074d71b45565a9a6d9c9b590e4ef6ff2664e
dm4nk <dim.xx2011@yandex.ru>
2021-11-14 18:04:36 +0400

    added read write building

--- src/main/java/model/buildings/net/client/BinaryClient.java
+++ src/main/java/model/buildings/net/client/BinaryClient.java
@@ -1,10 +1,39 @@
 package model.buildings.net.client;
 
-import java.io.IOException;
+import model.buildings.dwelling.Dwelling;
+import model.buildings.dwelling.DwellingFloor;
+import model.buildings.dwelling.Flat;
+import model.utilities.Buildings;
+
+import java.io.*;
 import java.net.Socket;
 
 public class BinaryClient {
+
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 8080);
+        DwellingFloor floor1;
+        DwellingFloor floor2;
+        DwellingFloor floor3;
+
+        Dwelling building1;
+
+        floor1 = new DwellingFloor(new Flat[]{new Flat(1, 1)});
+        floor2 = new DwellingFloor(new Flat[]{new Flat(1, 2), new Flat(2, 2)});
+        floor3 = new DwellingFloor(new Flat[]{new Flat(1, 3), new Flat(2, 3), new Flat(3, 3)});
+
+        building1 = new Dwelling(new DwellingFloor[]{floor1, floor2, floor3});
+
+
+        try (Socket socket = new Socket("localhost", 8080);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())))) {
+            System.out.println("Connected to server");
+            System.out.println("Writing building");
+            Buildings.writeBuilding(building1, out);
+            System.out.println("Reading size");
+            int size = in.read();
+            System.out.println("Size:" + size);
+        }
+
     }
 }
--- src/main/java/model/buildings/net/server/sequential/BinaryServer.java
+++ src/main/java/model/buildings/net/server/sequential/BinaryServer.java
@@ -1,14 +1,26 @@
 package model.buildings.net.server.sequential;
 
-import java.io.IOException;
+import model.buildings.Building;
+import model.utilities.Buildings;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class BinaryServer {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080);
-        Socket clientSocket = serverSocket.accept();
 
-        System.out.println("Clients connected");
+
+        try (ServerSocket serverSocket = new ServerSocket(8080);
+             Socket clientSocket = serverSocket.accept();
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())))) {
+            System.out.println("Clients connected");
+            Building building = Buildings.readBuilding(in);
+            System.out.println(building);
+
+            System.out.println("Writing size" + building.size());
+            out.write(building.size());
+        }
     }
 }
