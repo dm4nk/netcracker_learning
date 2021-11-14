c257ab646fdb0a3ce65d32b39b6437482207ddcd
dm4nk <dim.xx2011@yandex.ru>
2021-11-14 18:11:59 +0400

    fixed writeBuilding so that it won't close Socket

--- src/main/java/model/buildings/net/server/sequential/BinaryServer.java
+++ src/main/java/model/buildings/net/server/sequential/BinaryServer.java
@@ -10,12 +10,13 @@ import java.net.Socket;
 public class BinaryServer {
     public static void main(String[] args) throws IOException {
 
-
+        System.out.println("Waiting for client");
         try (ServerSocket serverSocket = new ServerSocket(8080);
              Socket clientSocket = serverSocket.accept();
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
              PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())))) {
             System.out.println("Clients connected");
+            System.out.println("Reading building:\n");
             Building building = Buildings.readBuilding(in);
             System.out.println(building);
 
