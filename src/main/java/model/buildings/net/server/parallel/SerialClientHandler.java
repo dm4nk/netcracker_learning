105e7f70abe00986ee612a211b5914e06afd6b8f
dm4nk <dim.xx2011@yandex.ru>
2021-11-15 15:47:14 +0400

    minor refactoring

--- src/main/java/model/buildings/net/server/parallel/SerialClientHandler.java
+++ src/main/java/model/buildings/net/server/parallel/SerialClientHandler.java
@@ -1,16 +1,11 @@
 package model.buildings.net.server.parallel;
 
 import lombok.NonNull;
-import model.buildings.Building;
-import model.buildings.net.server.utility.SomeServerUtilities;
-import model.exeptions.BuildingUnderArrestException;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 
 import static model.buildings.net.server.utility.SomeServerUtilities.executeSerialDataExchange;
 
--- src/main/java/model/buildings/net/server/sequential/SerialServer.java
+++ src/main/java/model/buildings/net/server/sequential/SerialServer.java
@@ -1,17 +1,10 @@
 package model.buildings.net.server.sequential;
 
-import model.buildings.Building;
-import model.buildings.net.server.parallel.SerialClientHandler;
-import model.buildings.net.server.utility.SomeServerUtilities;
-import model.exeptions.BuildingUnderArrestException;
-
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 
 import static model.buildings.net.server.utility.SomeServerUtilities.executeSerialDataExchange;
 
