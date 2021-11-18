a53e2dac0ad4930587f992f4afe808dcbed99f2d
dm4nk <dim.xx2011@yandex.ru>
2021-11-18 20:04:22 +0400

    fixed BinaryClient

--- src/main/java/model/buildings/net/client/BinaryClient.java
+++ src/main/java/model/buildings/net/client/BinaryClient.java
@@ -52,7 +52,7 @@ public class BinaryClient {
                 System.out.println("Reading price");
                 String price = in.readLine();
                 System.out.println(price);
-                outPrices.write(price);
+                outPrices.write(price + "\n");
             }
 
             System.out.println("Sending end message");
