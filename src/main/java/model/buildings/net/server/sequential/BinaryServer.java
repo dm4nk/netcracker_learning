29aac094ffb2c1b5d6177a29b5749898a296c219
dm4nk <dim.xx2011@yandex.ru>
2021-11-18 18:31:45 +0400

    fixing BinaryServer

--- src/main/java/model/buildings/net/server/sequential/BinaryServer.java
+++ src/main/java/model/buildings/net/server/sequential/BinaryServer.java
@@ -10,16 +10,19 @@ public class BinaryServer {
     public static final int PORT = 8080;
 
     public static void main(String[] args) throws IOException {
+        System.out.println("Waiting for client");
         while (true) {
-            System.out.println("Waiting for client");
             try (ServerSocket serverSocket = new ServerSocket(PORT);
+
                  Socket clientSocket = serverSocket.accept();
                  BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                  PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())))) {
+
                 System.out.println("Clients connected");
                 System.out.println("Reading size parameter:");
                 executeBinaryDataExchange(in, out);
             }
+
         }
     }
 }
